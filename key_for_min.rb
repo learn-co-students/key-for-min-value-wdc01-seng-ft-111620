e01d6e59e5e39e89b3c068d0ac6d638ffe8dafba
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-10-23 15:08:37 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  return nil if name_hash.empty?
+  array = []
+  name_hash.each {|key,value| array << value}
+  min = array[0]
+  array.each do |number| 
+    if number < min
+      min = number 
+    end
+  end 
+  name_hash.each {|key,value| return key if value == min}
 end
+
